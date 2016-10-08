c77ba4533d50da9c9af2b13e4d65f914bf74bc3e
PeterSchwab <peter.schwab@bfh.ch>
2016-10-08 09:11:50 +0200

    added some traversal methods to MyTree

--- src/examples/MyTree.java
+++ src/examples/MyTree.java
@@ -77,8 +77,8 @@ public class MyTree<E> implements Tree<E> {
 
 	@Override
 	public int numberOfChildren(Position<E> parent) {
-		// TODO Auto-generated method stub
-		return 0;
+		TNode p = checkAndCast(parent);
+		return p.children.size();
 	}
 
 	@Override
@@ -118,8 +118,12 @@ public class MyTree<E> implements Tree<E> {
 
 	@Override
 	public void remove(Position<E> p) {
-		// TODO Auto-generated method stub
-
+		TNode n = checkAndCast(p);
+		if (n!= root) {
+			n.parent.children.remove(n.myChildrenPos);
+		}
+		size--;
+		n.creator = null;
 	}
 
 	@Override
@@ -132,12 +136,12 @@ public class MyTree<E> implements Tree<E> {
 	public boolean isInternal(Position<E> p) {
 		// TODO Auto-generated method stub
 		return false;
+		
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
@@ -157,15 +161,70 @@ public class MyTree<E> implements Tree<E> {
 		while (it.hasNext()) print(it.next(),ind+"  ");
  	}
 
+	public void removeSubtree(Position<E> p){
+		TNode n = checkAndCast(p);
+		Iterator<TNode> it = n.children.elements();
+		while (it.hasNext()) remove(it.next());
+		remove(n);
+	}
+	
 	public static void main(String[] args) {
 		MyTree<String> t = new MyTree<>();
 		Position<String>p = t.createRoot("buch");
 		t.addChild(p, "kapitel 1");
 		Position<String>p2 = t.addChild(p, "kapitel 2");
-		t.addChild(p2, "kapitel 2.1");
+		Position<String>p3 = t.addChild(p2, "kapitel 2.1");
 		t.addChild(p, "kapitel 3");
 		t.addChild(p, "kapitel 4");
+		t.addChild(p3, "kapitel 2.1.1");
+		t.addChild(p3, "kapitel 2.1.2");
 		t.print();
+		t.removeSubtree(p3);
+		t.print();
+		System.out.println(height(t));
+		System.out.println(deepestPos(t).element());
+		
 	}
 
+	static int height(MyTree t){
+		if (t.size == 0) return 0;
+		return height(t, t.root());
+	}
+	
+	static class Temp {
+		int d;
+		Position deepest;
+	}
+	
+	static int maxDepth;
+	static Position deepestPos;
+	
+	static Position deepestPos(MyTree t){
+		if (t.size()==0) return null;
+		Temp tmp = new Temp();
+		tmp.d = 0;
+		tmp.deepest = t.root();
+		traverse(t,t.root(),0,tmp);
+		return tmp.deepest;
+	}
+	
+	static void traverse(Tree t, Position p, int depth,Temp tmp){
+		if (depth>tmp.d){
+			tmp.d = depth;
+			tmp.deepest = p;
+		}
+		Iterator<Position> it = t.childrenPositions(p);
+		while (it.hasNext()) traverse(t, it.next(), depth+1,tmp);
+		
+	}
+	
+	static int height(MyTree t, Position p){
+		int max = 0;
+		Iterator<Position> it = t.childrenPositions(p);
+		while (it.hasNext()){
+			int h = height(t,it.next());
+			if (max<h) max = h;
+		}
+		return max+1;
+	}
 }
